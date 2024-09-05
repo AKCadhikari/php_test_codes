88ce9573d8183939f767568d704462c2a4ed5587
Kaveen chandupa <aakchandupa@gmail.com>
2024-09-05 20:34:03 +0530

    Created test1.php
    Created test1.php for my first PHP code

---
+++ test1.php
@@ -0,0 +1,14 @@
+<?php
+    $shopA=8;
+    $shopA_price=75;
+    $shopB=5;
+    $shopB_price=60;
+
+    //to find the difference
+    $A=$shopA*$shopA_price;
+    $B=$shopB*$shopB_price;
+    $C=$A-$B;
+
+    echo "difference between two shops are $A-$B=$C"
+
+?>
