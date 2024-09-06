149a519e5ef5006d2f18ec1156004606b75019bf
Kaveen chandupa <aakchandupa@gmail.com>
2024-09-06 14:59:28 +0530

    new question

---
+++ test6.php
@@ -0,0 +1,9 @@
+<?php
+function frienddetail($name,$byear){
+    echo "your name is $name and birth year $byear <br>";
+}
+
+frienddetail("kaveen",2005);
+frienddetail("sasiru",2005);
+frienddetail("lakiya",2005);
+?>
