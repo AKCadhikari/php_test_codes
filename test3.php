8aad06f13d9c5fce09c104dcf70b3cbb29bdb342
Kaveen chandupa <aakchandupa@gmail.com>
2024-09-06 11:50:38 +0530

    created new program for practice

---
+++ test3.php
@@ -0,0 +1,16 @@
+<?php
+$marks=90;
+
+if($marks>=90){
+    echo("excellent");
+}
+elseif($marks>=75){
+    echo("good");
+}
+elseif($marks>=50){
+    echo("you passed");
+}
+else{
+    echo("you failed");
+}
+?>
